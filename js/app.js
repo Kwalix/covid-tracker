36c8f12b09b55c9cabfc8964cf76666eba391b57
Kwalix <lyk4n@protonmail.com>
2020-07-20 19:09:45 +0200

    First commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$.ajax({
+    type: "GET",
+    url: "https://covid2019-api.herokuapp.com/v2/current",
+    data: "data",
+    dataType: "dataType",
+    success: function (response) {
+        
+    }
+});
