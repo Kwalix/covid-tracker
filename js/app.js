c07ee8033a5956aabf051908640b08956590eb46
Kwalix <lyk4n@protonmail.com>
2020-07-21 19:09:24 +0200

    index.html ended and example data for chart

--- js/app.js
+++ js/app.js
@@ -1,9 +1,44 @@
-$.ajax({
+/*
+$('chLine').ajax({
     type: "GET",
     url: "https://covid2019-api.herokuapp.com/v2/current",
     data: "data",
     dataType: "dataType",
-    success: function (response) {
-        
+    success: (res) => {
     }
 });
+*/
+let chartData = {
+    labels: ["Confirmed", "Deaths", "Recovered", "Active"],
+    datasets: [{
+      data: [1234, 6571, 3654, 9853],
+    }]
+  };
+  
+let chLine = document.getElementById('chLine');
+if (chLine) {
+    new Chart(chLine, {
+    type: 'line',
+    data: chartData,
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+    });
+  } else {
+    let noConexion = () => {
+        let coAlert = $('<div></div>').addClass("alert alert-danger block").text(' Error while requesting data');
+        let alertIcon = $('<i></i>').addClass("fa fa-bug")
+        $('#chrt').prepend(coAlert);
+        $('.alert').prepend(alertIcon);
+      }
+      noConexion();
+  }
